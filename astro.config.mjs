206d86c211e5db8bcdf2db29423bbe91788a042b
mohansky <38560439+mohansky@users.noreply.github.com>
2025-11-02 13:09:28 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,22 @@ import mdx from "@astrojs/mdx";
 
 import netlify from "@astrojs/netlify";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yourdomain.com", // Replace with your actual domain
+  site: "https://maarif-haque.netlify.app", // Replace with your actual domain
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.9,
+      lastmod: new Date("2025-11-01"),
+    }),
+  ],
   adapter: netlify(),
 });
